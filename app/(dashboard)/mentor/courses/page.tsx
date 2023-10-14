89d662871ec42954962f80b6410ef7301efdc578
wxwilliamc <chongwx-pm18@student.tarc.edu.my>
2023-10-14 21:21:30 +0800

    add data table

--- app/(dashboard)/mentor/courses/page.tsx
+++ app/(dashboard)/mentor/courses/page.tsx
@@ -1,15 +1,28 @@
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import React from 'react'
+import { columns } from './_components/columns'
+import { DataTable } from './_components/data-table'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import { db } from '@/lib/db'
 
-const Courses = () => {
+const Courses = async () => {
+  const { userId } = auth();
+  if(!userId) redirect('/')
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc"
+    }
+  })
+  
   return (
     <div className='p-6'>
-      <Link href="/mentor/create">
-        <Button>
-          New Course
-        </Button>
-      </Link>
+      <DataTable columns={columns} data={courses}/>
     </div>
   )
 }
